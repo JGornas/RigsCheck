ab64baf115616403d9c22edb56c731ecd7f9eccb
Jake <jjgornas@gmail.com>
2021-05-26 23:24:27 +0200

    - refactor

--- bin/Exchanger.py
+++ bin/Exchanger.py
@@ -4,14 +4,6 @@ from bs4 import BeautifulSoup
 
 class Exchanger:
     def __init__(self):
-        self.eth_price_usd = ""
-        self.rvn_price_usd = ""
-        self.usd_to_pln_ratio = ""
-        self.eth_price_pln = ""
-        self.rvn_price_pln = ""
-        self.update()
-
-    def update(self):
         self.eth_price_usd = requests.get('https://api.ethermine.org/poolStats').json()['data']['price']['usd']
         self.rvn_price_usd = requests.get("https://api-ravencoin.flypool.org/poolStats").json()['data']['price']['usd']
 
